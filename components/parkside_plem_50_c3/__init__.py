eb8e25d64cd700317e333c95c5e5455d8c6f4752
Pavel Rybnicek <Pavel.Rybnicek@email.cz>
2024-01-21 10:45:24 +0100

    Zapnuti laseru

--- components/parkside_plem_50_c3/__init__.py
+++ components/parkside_plem_50_c3/__init__.py
@@ -27,7 +27,7 @@ CONFIG_SCHEMA = cv.Schema({
 
 }).extend(cv.polling_component_schema('10s')).extend(uart.UART_DEVICE_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
