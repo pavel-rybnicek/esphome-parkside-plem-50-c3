7fcf3a951322800b5ca754c2a3d2a5ee8261fbf1
Pavel Rybnicek <Pavel.Rybnicek@email.cz>
2024-01-25 21:57:21 +0100

    Zapinani

--- components/parkside_plem_50_c3/parkside_plem_50_c3.cpp
+++ components/parkside_plem_50_c3/parkside_plem_50_c3.cpp
@@ -184,12 +184,12 @@ void ParksidePlem50C3Component::update() {
   //digitalWrite(13, 1);
   delay (100);
   digitalWrite(PIN_VYPINAC, 1);
-  delay (1000);
+  delay (400);
   digitalWrite(PIN_KLAVESNICE, 0);
-  delay (500);
+  delay (400);
   digitalWrite(PIN_KLAVESNICE, 1);
 
-  delay (2000);
+  delay (3000);
   //digitalWrite(PIN_KLAVESNICE, 1);
   //delay (3000);
   //digitalWrite(PIN_VYPINAC, 1);*/
